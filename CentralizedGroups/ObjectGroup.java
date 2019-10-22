81202ccb58b15bd9437d9338c7300edc06a8f9a4
Miguel <miguelsrosa97@gmail.com>
2019-10-22 14:28:21 +0200

    more attribute renaming, isMember done

--- CentralizedGroups/ObjectGroup.java
+++ CentralizedGroups/ObjectGroup.java
@@ -16,6 +16,7 @@ public class ObjectGroup {
     String oalias;
     int gid;    /* group id */
     int oid;    /* owner id */
+    private int counter;
     LinkedList<GroupMember> members;
     
     public ObjectGroup(String galias, int gid, String oalias, int oid) {
@@ -23,19 +24,30 @@ public class ObjectGroup {
         this.oalias = oalias;
         this.gid = gid;
         this.oid = oid;
+        this.counter = 1;
         addMember(oalias);
     }
     
-    public GroupMember isMember(String userAlias) {
-        
+    public GroupMember isMember(String alias) {
+        /* buscar miembro en la lista */
+        for (GroupMember member : members) {
+            if (member.alias.equals(alias)) return member;
+        }
         return null;
     }
     
-    public GroupMember addMember(String userAlias) {
+    public GroupMember addMember(String alias) {
+        /* si ya existe un miembro con el mismo alias, devolver null */
+        if (isMember(alias) == null) return null;
+        /* si no, añadir miembro nuevo */
+        members.add(new GroupMember(alias, this.oalias, counter, this.gid));
+        /*                                 ^ nombre del propietario??     */
+        counter++;
+        /* TODO: figure this return out */
         return null;
     }
     
-    public boolean removeMember(String userAlias) {
+    public boolean removeMember(String ualias) {
         return false;
     }
     

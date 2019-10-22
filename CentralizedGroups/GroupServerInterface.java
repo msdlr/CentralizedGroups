bbfe17d6cc80a582b99cb484b6131b21defba900
Miguel Sánchez de la Rosa <miguelsrosa97@gmail.com>
2019-10-22 13:25:21 +0200

    delcarada interfaz del servidor

--- CentralizedGroups/GroupServerInterface.java
+++ CentralizedGroups/GroupServerInterface.java
@@ -5,10 +5,22 @@
  */
 package CentralizedGroups;
 
+import java.util.LinkedList;
+
 /**
  *
  * @author Miguel
  */
 public interface GroupServerInterface {
-    
+    int createGroup (String galias, String oalias, String ohostname);
+    int findGroup (String galias);
+    String findGroup (int gid);
+    boolean removeGroup (String galias, String oalias);
+    GroupMember addMember (String galias, String alias, String hostname);
+    boolean removeMember (String galias, String alias);
+    GroupMember isMember (String galias, String alias);
+    boolean StopMembers (String galias);
+    boolean AloowMembers (String gid);
+    LinkedList<String> ListMembers (String galias);
+    LinkedList<String> ListGroup();
 }

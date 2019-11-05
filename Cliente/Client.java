8f4ef5a6c6ba5d8dcbc2f087a50d5778b4925f31
Guillermo Fernandez <GuillermoTomas.Fernandez@alu.uclm.es>
2019-11-05 13:55:02 +0100

    client main things

--- Cliente/Client.java
+++ Cliente/Client.java
@@ -7,6 +7,7 @@ package Cliente;
 
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
+import java.util.Scanner;
 
 /**
  *
@@ -15,5 +16,33 @@ import java.rmi.server.UnicastRemoteObject;
 public class Client extends UnicastRemoteObject implements ClientInterface{
     Client() throws RemoteException {
         
+    }
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String command = "";
+        boolean menu = true;
+        
+        /* establecer conexion remota o local */
+        System.out.println("Conexión remota o local? (l/<dir. ip>)");
+        command = scanner.nextLine();
+        if (command.equals("l")) {
+            host = "127.0.0.1";
+        } else {
+            host = command;
+        }
+        
+        /* menu */
+        System.out.println("Para consultar todas las acciones disponibles, escribe /?");
+        System.out.print(">> ");
+        while (menu) {
+            command = scanner.next("/*");
+            switch (command) {
+                case "/?":
+                    System.out.println("help");
+                    break;
+                    
+            }
+        }
     }
 }

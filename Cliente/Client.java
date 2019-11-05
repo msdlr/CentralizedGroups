8cb607d2cd7debf3bb4f3b3f34b28aa909e23de9
Guillermo Fernandez <GuillermoTomas.Fernandez@alu.uclm.es>
2019-11-05 14:50:05 +0100

    Atributos del cliente

--- Cliente/Client.java
+++ Cliente/Client.java
@@ -6,7 +6,10 @@
 package Cliente;
 
 import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
+import javax.rmi.CORBA.Stub;
 
 /**
  *
@@ -14,6 +17,11 @@ import java.rmi.server.UnicastRemoteObject;
  */
 public class Client extends UnicastRemoteObject implements ClientInterface{
     Client() throws RemoteException {
-        
+        //Localizar el servidor en el registro
+        String host=null;
+        //Registro donde buscar el servidor
+        Registry registro; 
+        //Proxy para los métodos del servidor
+        Stub GroupServerInterface; 
     }
 }

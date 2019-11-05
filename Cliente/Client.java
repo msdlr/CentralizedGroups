3cb47d4eadec263dc99653570cf57c066953b77c
Guillermo Fernandez <GuillermoTomas.Fernandez@alu.uclm.es>
2019-11-05 14:32:14 +0100

    principio de func4

--- Cliente/Client.java
+++ Cliente/Client.java
@@ -6,6 +6,7 @@
 package Cliente;
 
 import CentralizedGroups.GroupServerInterface;
+import static java.lang.System.exit;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
@@ -24,7 +25,7 @@ public class Client extends UnicastRemoteObject implements ClientInterface {
 
     Client() throws RemoteException {}
 
-    public static void main(String[] args) {
+    public void main(String[] args) {
         //Localizar el servidor en el registro
         String host = null;
         //Registro donde buscar el servidor
@@ -72,8 +73,56 @@ public class Client extends UnicastRemoteObject implements ClientInterface {
             command = scanner.nextLine();
             switch (command) {
                 case "":
+                    
+                case "4":
+                    func4(host, registro, proxy);
 
+                default:
+                    System.out.println("Error, saliendo");
+                    break;
             }
+        }
+    }
+    private void func4(String host, Registry registro, GroupServerInterface proxy){
+        //Variables
+        String st = "";
+        Scanner s = new Scanner(System.in);
+        int gid = 0;
+        String galias = null;
+        
+        //Necesitamos el gid del grupo y si es alta o baja
+        //Buscar grupo
+        System.out.println("Introduce el alias del grupo");
+        st = s.nextLine();
+        System.out.println("Buscando grupo"+st);
+        
+        gid = proxy.findGroup(galias);
+        if(gid == -1){
+            //Si el grupo no se ha encontrado
+            System.out.println("Grupo " + galias + " no encontrado");
+        }
+        else{
+            //Si el grupo se ha encontrado
+            System.out.println("GRUPO:" + st + " con  GID" + gid);
+            
+            System.out.println("[B]loquear altas/bajas \n [D]esbloquear altas/bajas ");
+            
+            st = s.nextLine();
+            switch(st){
+                case "D":
+                case "d":
+                    proxy.AllowMembers(st);
+                    System.out.println("Bloqueadas las altas/bajas en el grupo");
+                    break;
+                case "B":
+                case "b":
+                    proxy.StopMembers(st);
+                    System.out.println("Bloqueadas las altas/bajas en el grupo");
+                    break;
+                default:
+                    System.out.println("ERROR");
+            }
+            
         }
     }
 }

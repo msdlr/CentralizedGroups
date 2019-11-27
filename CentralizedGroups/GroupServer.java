64e86f860140c6912576f7ee984a4258abed5fb8
Miguel Sánchez <miguelsrosa97@gmail.com>
2019-11-27 18:19:51 +0100

    Pathname relativo al proyecto

--- CentralizedGroups/GroupServer.java
+++ CentralizedGroups/GroupServer.java
@@ -47,9 +47,8 @@ public class GroupServer extends UnicastRemoteObject implements GroupServerInter
     /* MAIN */
     public static void main(String args[]) throws RemoteException {
         //Fichero de política
-        System.setProperty("java.security.policy", "C:\\Users\\Miguel\\Desktop\\CentralizedGroups\\src\\CentralizedGroups\\seguridad.txt");
-        //System.setProperty("java.security.policy", "C:\\Users\\usuario\\Desktop\\CentralizedGroups\\src\\CentralizedGroups\\seguridad.txt");
-        System.setProperty("java.rmi.server.hostname","192.168.0.28");  // <-- use server address here
+        System.setProperty("java.security.policy", "src/CentralizedGroups/seguridad.txt");
+        //System.setProperty("java.rmi.server.hostname","192.168.0.28");  // <-- use server address here
         GroupServer server = new GroupServer();
        
         //Si no se ejecuta aquí da excepción
